39bdc1442112a859b25b047c1d6a6c89ad88aad3
Logan Gilmour <logangilmour@Logans-MacBook-Air.local>
2014-06-21 22:55:26 -0600

    Ostensibly working engine

--- js/main.js
+++ js/main.js
@@ -1,54 +1,80 @@
 var horizon=0;
 var textHeight;
-var beat=250;
+var beat=400;
 var wait=0;
 var stop=false;
-var act = null;
 var dead = false;
+var testing = false;
 var $lines;
+var interval;
+    var letters=["A","O","E","U"];
+var off=4-(window.actions!==undefined?actions.length:0);
+
+
 $(function(){
     $('body').append("<div id='blackout'>Press Spacebar to Retry...</div>");
+    $("<div id='red'></div>").appendTo('body').hide();
+    $("<div id='grey'></div>").appendTo('body').hide();
     $('body').append("<div id='weapons-holder'><span id='weapons'>Weapons</span></div>");
     $('#holder').offset({left:$(window).width()/2-200});
-    var letters=["A","S","D","F"];
-    var off=4-actions.length;
     for(var i=0;i<4;i++){
-	console.log(i-off);
 	$("<div class='action'>"+(
 	  ((i-off)>=0)?("<div class='word'>"+actions[i-off].name+"</div>"):"")+
 	 "<div class='letter'>["+letters[i]+"]</div></div>").appendTo("body").css({left:(25*i)+'%'});
     }
     $(".challenge").each(function(i,el){
 	var test = tests[Math.floor(Math.random()*tests.length)%tests.length];
-	$(el).after("<p b='2' class='new s'>"+test.start+"</p><p b='2' class='e'>"+test.end+"</p>").remove();
+	$(el).after("<p b='2' test='"+test.needs+"' class='new s'>"+test.start+"</p><p b='2' class='e'>"+test.end+"</p>").remove();
     });
     $lines = $("#holder").children();
     $lines.each(function(i,line){process(line);});
-
+    $(document).keyup(function(evt){$('#red').hide(); $('#grey').hide();});
     $(document).keydown(function(evt){
+		var action = actions[letters.indexOf(String.fromCharCode(evt.keyCode).toUpperCase())-off];
+		console.log(JSON.stringify(action));
+
 	if(dead && evt.keyCode==32)
 	{
 	    evt.preventDefault();
 	    location.reload();
+	    return;
 	}
-	    
-	if(!act){
-	 
-	var action = actions[String.fromCharCode(evt.keyCode)];
-	    if(action){
-
-		}
-	if(actions){
-	    
-	    var $action = $(action).insertAfter($lines[horizon-1]);
-	    $lines = $("#holder").children();
+	if(evt.keyCode==32){
+	    evt.preventDefault();
+	    reset();
+	    tick();
+	}
+	if(testing && action){
+	    if(testing==action.name){
+		$('#grey').show();
+		 var $action = $("<p b='2'>"+action.text+"</p>").insertAfter($lines[horizon-1]);
+		$($lines[horizon]).remove();
 	    process($action);
-	}
+		$lines = $("#holder").children();
+
+	    }else{
+		$('#red').show();
+		 var $action = $("<p b='2'>"+action.text+"</p>").insertAfter($lines[horizon-1]);
+		
+		process($action);
+		 $action = $("<p b='2'>"+action.fail+"</p>").insertAfter($action);
+		
+		process($action);
+		$lines = $("#holder").children();
+
+	    }
+	    testing=null;
 	}
     });
     textHeight = 30;
-    setInterval(tick,beat);
+    reset();
 });
+
+function reset(){
+    if(interval)clearInterval(interval);
+    wait=0;
+    interval = setInterval(tick,beat);
+}
 
 function process(line){
     var $line = $(line);
@@ -75,10 +101,14 @@ function tick(){
 	var b = $line.attr("b");
 	if(!b)b=4;
 	wait=b;
+	testing = $line.attr("test");
+	
 	var margin = $line.hasClass("new")?textHeight:0;
 	$line.css({display:'block','line-height':0,'marginTop':0});
-	$line.animate({opacity:1,'line-height':textHeight,'marginTop':margin},
-		      beat);
+	if(testing){
+	    $line.animate({opacity:1,'line-height':textHeight,'marginTop':margin},50);
+	}
+	else $line.animate({opacity:1,'line-height':textHeight,'marginTop':margin}, beat);
 		if($line.hasClass("e")){
 	    $('#blackout').fadeIn(3000);
 	    stop=true;
